9c28cf819b8b8eaad6828156f2c9b42514f88fc1
Abdul Hameed <raza2022@gmail.com>
2018-12-11 19:56:16 +0500

    apollo client connected to server

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import {Meteor} from 'meteor/meteor'
-import { render } from 'react-dom'
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
 
 import App from '/imports/ui/App'
 
+const httpLink = new HttpLink({
+    uri: Meteor.absoluteUrl('graphql')
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+   link: httpLink,
+   cache
+});
+
+const ApolloApp = () => (
+    <ApolloProvider client={client} >
+        <App />
+    </ApolloProvider>
+);
+
 Meteor.startup(() => {
-    render(<App />, document.getElementById("app"));
+    render(<ApolloApp />, document.getElementById("app"));
 });
