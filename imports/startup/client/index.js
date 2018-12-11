c7892707f0d9836e1616182b52c750d9a23b6a6b
Abdul Hameed <raza2022@gmail.com>
2018-12-11 18:47:50 +0500

    graphql packages added app modified

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Meteor} from 'meteor/meteor'
 import { render } from 'react-dom'
 
-const App = () => <h1>Hello</h1>;
+import App from '/imports/ui/App'
 
 Meteor.startup(() => {
     render(<App />, document.getElementById("app"));
---
+++ imports/ui/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => <h1>Hello Apollo</h1>;
+
+export default App
