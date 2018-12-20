2ff477f516f5033a8e6986a86dffcfa921e3bd5d
Abdul Hameed <raza2022@gmail.com>
2018-12-20 17:31:54 +0500

    extended and separated queries and schemas

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -6,31 +6,16 @@ import ResolutionsSchema from '../../api/resolutions/resolutions.graphql';
 import UserSchema from '../../api/users/User.graphql';
 import UserResolvers from '/imports/api/users/resolvers'
 import ResolutionsResolvers from '/imports/api/resolutions/resolvers'
-
-
-const testSchema = `type Query {
-    hi : String
-    resolutions: [Resolution]
-    user: User
-}`;
+//hii
 
 
 
 const typeDefs = [
-    testSchema,
     ResolutionsSchema,
     UserSchema
 ];
 
-const resolver = {
-    Query: {
-        hi(){
-            return "Hello Apollo!"
-        }
-    }
-};
-
-const resolvers = merge(resolver, ResolutionsResolvers, UserResolvers);
+const resolvers = merge(ResolutionsResolvers, UserResolvers);
 
 const schema = makeExecutableSchema({
     typeDefs,
